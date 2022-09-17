bfa62e23eeee1465a7ac839ab1ac22134c3b1a9d
nithesh1997 <102368610+nithesh1997@users.noreply.github.com>
2022-09-17 15:36:33 +0530

    Add files via upload

---
+++ src/components/CartItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Cart.css";
+
+const CartItem = ({ name, quantity, total, price, id }) => {
+
+  return (
+    <div className="cartItem">
+      <h2> {name}</h2>
+      <p>${price} /-</p>
+      <p>x{quantity}</p>
+      <article>Total ${total}</article>
+      <button className="cart-actions">
+        -
+      </button>
+      <button className="cart-actions" >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default CartItem;
